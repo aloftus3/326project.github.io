2595540ff2bf9671f3132e00e533649d6282a24b
Marzenah <37334652+Marzenah@users.noreply.github.com>
2019-12-06 20:50:17 -0500

    Update PropublicaConnection.py

--- PropublicaConnection.py
+++ PropublicaConnection.py
@@ -1,11 +1,1 @@
-##downloaded
-###### Initial connection to Propublica  #######
-import requests
 
-# This is the get function he set up in class
-
-def get(url):
-    headers = {"X-API-KEY": "WWSGHLJjWxC2m9tznYILCKy1xtmkvnxxdo8nEBt8"}
-    url = "https://api.propublica.org/congress/v1" + url
-    data = requests.get(url, headers=headers).json()
-    return data['results']
