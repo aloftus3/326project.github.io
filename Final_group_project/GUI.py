7c64b9a796b4f4f4bf320442eb9ecdb296f5af2e
Marzenah <37334652+Marzenah@users.noreply.github.com>
2019-12-07 02:56:29 -0500

    Update GUI.py
    improvements and some suggestions for plugging in other search results

--- Final_group_project/GUI.py
+++ Final_group_project/GUI.py
@@ -3,7 +3,10 @@ from PIL import ImageTk, Image
 import numpy as np
 import matplotlib.pyplot as plt
 import search3
+# !!!! here we would import search1 (some file search1.py that brings results for search1 based on the input of last and first name)
+# !!!! here we would import search2 (some file search2.py that brings results for search2 based on the input of state's two-letter code)
 from search_modules import*
+
 
 def show_data():
     search_num = choice_var.get()
@@ -13,16 +16,22 @@ def show_data():
         results = Tk()
         results.title('  Search Results ')
         results.iconbitmap('small_Logo.ico')
-        results.geometry('580x580')
-        txt_box = Text(results, width=500, height=350, wrap=WORD).grid(row=1)
-
-        if search_num == 2:
-            first = str(first_name.get())
-            last = str(last_name.get())
-
-        elif search_num == 1:
-            state = str(state_code.get())
-
+        
+        txt_box = Text(results, wrap=WORD).pack()
+        txt_box.pack()
+        
+        if search_num == 1:
+            first_name = str(entry_first_name.get())
+            last_name = str(entry_last_name.get())
+            
+            # !!! result = search1.SomeMethod(first_name, last_name)
+            # !!! txt_box.insert(END, result)
+            
+        elif search_num == 2:
+            state_code = str(entry_state.get())
+            
+            # !!! result = search2.SomeMethod(state_code)
+            # !!! txt_box.insert(END, result)
         else:
             pass
 
@@ -40,21 +49,33 @@ label_main = Label(root, pady = 10, text='Choose search type', font='Halvetica 1
 label_main.grid(row=1, columnspan=3)
 
 choice_var = IntVar()
+last_name = StringVar()
+first_name = StringVar()
+state_code = StringVar()
 
 rd1 = Radiobutton(root, text='1: Search current representative by name', variable=choice_var, value=1)
 rd1.grid(row=2, column=0, padx=12, sticky=W)
-label1_search1 = Label(root, text="Enter last name: ").grid(row=2, column=1, sticky=W)
-last_name = StringVar()
-entry_last_name = Entry(root, bg="light grey").grid(row=2, column=2, sticky=W)
-label2_search1 = Label(root, text="Enter first name: ").grid(row=3, column=1, sticky=W)
-first_name = StringVar()
-entry_first_name = Entry(root, bg="light grey").grid(row=3, column=2, sticky=W)
+
+label1_search1 = Label(root, text="Enter last name: ")
+label1_search1.grid(row=2, column=1, sticky=W)
+
+entry_last_name = Entry(root, bg="light grey")
+entry_last_name.grid(row=2, column=2, sticky=W)
+
+label2_search1 = Label(root, text="Enter first name: ")
+label2_search1.grid(row=3, column=1, sticky=W) 
+
+entry_first_name = Entry(root, bg="light grey")
+entry_first_name.grid(row=3, column=2, sticky=W)
 
 rd2 = Radiobutton(root,text='2: Get list of Congressional representatives for a state', variable=choice_var, value=2)
 rd2.grid(row=5, column=0, padx=12, sticky=W)
-label1_search2 = Label(root, text="Enter two-letter code for state: ").grid(row=5, column=1, pady=15, sticky=W)
-state_code = StringVar()
-entry_state = Entry(root, justify=LEFT, bg="light grey").grid(row=5, column=2, pady=15, sticky=W)
+
+label1_search2 = Label(root, text="Enter two-letter code for state: ")
+label1_search2.grid(row=5, column=1, pady=15, sticky=W)
+
+entry_state = Entry(root, justify=LEFT, bg="light grey")
+entry_state.grid(row=5, column=2, pady=15, sticky=W) 
 
 rd3 = Radiobutton(root, text='3: Get historical data on average age in Congress', variable=choice_var, value=3)
 rd3.grid(row=7, column=0, padx=12, sticky=W)
