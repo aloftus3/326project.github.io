c79d317625992219ea3563aad325714db9fd4255
Jonah Rivera <54821166+jrivera115@users.noreply.github.com>
2019-12-09 13:38:50 -0500

    Removed key

--- PropublicaConnection.py
+++ PropublicaConnection.py
@@ -8,7 +8,7 @@ import string
 
 # This is the get function he set up in class
 def get(url):
-    headers = {"X-API-KEY": "WWSGHLJjWxC2m9tznYILCKy1xtmkvnxxdo8nEBt8"}
+    headers = {"X-API-KEY": "ENTER API KEY HERE"}
     url = "https://api.propublica.org/congress/v1" + url
     data = requests.get(url, headers=headers).json()
     return data['results']
